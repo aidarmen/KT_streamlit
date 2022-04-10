4c55abef5440d0ac3233bf203853098f1f22a072
Aidar <a87077380001@gmail.com>
2022-04-10 20:05:44 +0600

    clear input and read from google sheet

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import numpy as np
 import datetime
 import gspread
 
+cred = {
+    "type": "service_account",
+    "project_id": "focused-elysium-340604",
+    "private_key_id": "b4d9ed4edcf1403742b7783bff2a3cbf2ea4e6f8",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDIcsuFOPcIGXUO\nGgvuI7jEY0c2DHIA29pjNSY9MWPo0NlfYwOnOImpRdpEV+5LsdGTbCThfG+TZxfW\nmxJyt+HSbjycl6SIDmbKzZN8bgwU4KyxrsBU2hfiB5PjYKgOwKaAt+/QuW3ZC2aG\n9DeMS906O0zO5k3acxMgKWPjv3PMFjyen4AFON+f256PE07aqJpLYmRU1g4kLAlX\nmLwpYEygik8EJfKwBevwtxJjVu8fiaJjAo8X6UMmLieH2EZ5K0tPsji7MhKZwvy9\ninbewE5MerrI2AsIyZr0UAfeaom04UAynHtRGt+5IKxGQilHqhzJjiVpfk4hNi7E\nWExCb90vAgMBAAECggEAUQYuBDTpjgmUSlX2+pTP6/phX5SaRISZ+z4RreyYARTi\nuO7yHBb7dkP5HWUZutku6NNu+QJuq3uhpCrcwyhXDNNohre5VxHTNSjx8/sbwRis\nVcpYjjP2MXR0sBVy+TugUimHs6wwnepA0E5JYdmMnCw6OOvZDnMziO2SJg5OhXDE\nfFBNp1jA5uYri5J1KkaYnLBns8BGi26GHdVpEa5J+MGb24h04nuU5HfafpMP+Vty\nhXp43tnpsdOf75QNkFg5JR3QNzYsd3J6OkQj6MtLCbiWVUMtcZWYLt/sT9kusAz2\nj96VTrRKZx6p5+JEmPuVqKaWRvTYpN0aJ3YXTilQ+QKBgQDlTjbwDS/obF3VXX4e\nR5fdr1RNQUU0MdPM8tuatwrkKakcgfLZq/9le5f1mwOWKSQokUvZFb0OjAKOqZ43\n5wp1tM/UP6mF59SjJeW1lyf4aSoX4EypVDZassxGw+z+nPryo55s14br0zp+OFkt\n2azC/7S68eEri8tCVscElkcmVQKBgQDfyJP9veT+vf1/HVPlGpdBh8zCkPO3pzWE\nzMNTdIYY0OkOvfTl+4bQ+7DA+T9PN6ubMENVSp0SXcxfrTQBj0ePxapvzGeg7Lwm\ngMkUOTatGjLSzYyGWi5Tc/ynkIvq7PUrXsJ6dqnUiRa15pi9eTeRbfb/PmJps4qw\nFSl3LTYRcwKBgQDMSBbXWtgwVsJtgAIIVb2syDLxNFHDwxQuhLkxpxoz1A2NkRNZ\n6kn4Ddh9/OCAGRzWa0LIHf60g/UPRXrn0JPX8wIn06Nh4PvlcLCpSwVX39CZPsDt\nJVHbEWi9LnTnC9Dg8vUDgCr50s9MfUStfTvU6NsIpM317m9hJU/d/UdblQKBgQCt\n46FDoYRIgn9xf+uJ8iColgqBAbuv8KlAmRAKUFhG/kaq4uZisxFuYLVXLhaIytx5\nCYa4xvLIq8Q8cE0iDSDxUgp/NxkrTeliDd6x7UdUOTBW58wTNg4bYNxppINkuF5G\nJir+mCidcy0HibkUOhX41rcAXYyBUDcrXjZLWgFSiwKBgQCT79Q4GIN0MqJEvQxs\nvUfMF96gsJ9cEGGjROtw/qlpaPJBhpmNSuHc6gKAxLDneIdt5nBxyo+lZOpzMIve\ntwnocYa/D8RCOYBX1S3pecI72ss82N+Ek/o2+PNt3nbZ+wxBhNlOIMiH3LxKOsLM\nuzNty1dOJ97/L0ED5IZ9sESCwg==\n-----END PRIVATE KEY-----\n",
+    "client_email": "temp-606@focused-elysium-340604.iam.gserviceaccount.com",
+    "client_id": "113559229310896807100",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/temp-606%40focused-elysium-340604.iam.gserviceaccount.com"
+}
+
+@st.cache
+def long_running_function():
+    sa = gspread.service_account_from_dict(cred)
+    sh = sa.open("KT_DATA_ANKETA")
+    st.session_state.sh = sa.open("KT_DATA_ANKETA")
+    wks = sh.worksheet('map')
+
+    st.session_state.df = pd.DataFrame(wks.get_all_records()).astype('str')
+
+    # return pd.DataFrame(wks.get_all_records())
+# df = pd.read_csv('map.csv')
+
+
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
@@ -11,10 +37,8 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-df = pd.read_csv('map.csv')
 
-# Using the "with" syntax
-# with st.form(key='my_form'):
+
 
 
 q = {
@@ -101,6 +125,8 @@ def check_password():
 
 if check_password():
 
+    long_running_function()
+
 
     st.date_input(
     q[0],
@@ -129,13 +155,15 @@ if check_password():
         'Южная региональная дирекция телекоммуникаций'
     ]
 
+
+
     st.selectbox(
         q[3],
         options_rdt,
         key='a3')
 
-
-    st.session_state.options_region = np.unique(df.loc[df['rdt'] == st.session_state.a3, 'region'].to_numpy())
+    st.session_state.options_region = np.unique(
+        st.session_state.df.loc[st.session_state.df['rdt'] == st.session_state.a3, 'region'].to_numpy())
 
 
     st.selectbox(
@@ -144,7 +172,9 @@ if check_password():
     key='a4')
 
 
-    st.session_state.options_town = np.unique(df.loc[(df['rdt'] == st.session_state.a3 ) & (df['region'] == st.session_state.a4 ), 'isb_town'].to_numpy())
+    st.session_state.options_town = np.unique(
+        st.session_state.df.loc[(st.session_state.df['rdt'] == st.session_state.a3 ) &
+                                (st.session_state.df['region'] == st.session_state.a4 ), 'isb_town'].to_numpy())
 
     st.selectbox(
          q[5],
@@ -188,9 +218,9 @@ if check_password():
      st.session_state.option_street_type,
      key='a7')
 
-    st.session_state.options_street = np.unique(df.loc[(df['rdt'] == st.session_state.a3) &
-                                                       (df['region'] == st.session_state.a4) &
-                                                       (df['isb_town'] == st.session_state.a5),
+    st.session_state.options_street = np.unique(st.session_state.df.loc[(st.session_state.df['rdt'] == st.session_state.a3) &
+                                                       (st.session_state.df['region'] == st.session_state.a4) &
+                                                       (st.session_state.df['isb_town'] == st.session_state.a5),
                                                        'isb_street'].to_numpy())
 
     st.selectbox(
@@ -370,6 +400,7 @@ if check_password():
     # 38: 'Технологии ШПД',
 
     st.session_state.internet_tech = [
+    '',
     'нету',
     'DSL',
     'FTTh',
@@ -384,6 +415,7 @@ if check_password():
     # 39: 'Конкуренты',
 
     st.session_state.oppositions = [
+    '',
     'нету',
     'Beeline',
     'AlmaTV',
@@ -394,6 +426,7 @@ if check_password():
     st.multiselect(q[39],
     options =st.session_state.oppositions,
     key='a39')
+
 
     # 40: 'Технологии ШПД Конкурентов',
     st.multiselect(q[40],
@@ -413,7 +446,7 @@ if check_password():
 
     # 42: 'Мобильный ШПД',
     st.session_state.mob_network = [
-    'нету',
+    '',
     '2G',
     '3G',
     '4G'
@@ -443,40 +476,37 @@ if check_password():
     key='a45')
 
 
-    if st.button(label='отправить'):
 
 
-        cred = {
-            "type": "service_account",
-            "project_id": "focused-elysium-340604",
-            "private_key_id": "b4d9ed4edcf1403742b7783bff2a3cbf2ea4e6f8",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDIcsuFOPcIGXUO\nGgvuI7jEY0c2DHIA29pjNSY9MWPo0NlfYwOnOImpRdpEV+5LsdGTbCThfG+TZxfW\nmxJyt+HSbjycl6SIDmbKzZN8bgwU4KyxrsBU2hfiB5PjYKgOwKaAt+/QuW3ZC2aG\n9DeMS906O0zO5k3acxMgKWPjv3PMFjyen4AFON+f256PE07aqJpLYmRU1g4kLAlX\nmLwpYEygik8EJfKwBevwtxJjVu8fiaJjAo8X6UMmLieH2EZ5K0tPsji7MhKZwvy9\ninbewE5MerrI2AsIyZr0UAfeaom04UAynHtRGt+5IKxGQilHqhzJjiVpfk4hNi7E\nWExCb90vAgMBAAECggEAUQYuBDTpjgmUSlX2+pTP6/phX5SaRISZ+z4RreyYARTi\nuO7yHBb7dkP5HWUZutku6NNu+QJuq3uhpCrcwyhXDNNohre5VxHTNSjx8/sbwRis\nVcpYjjP2MXR0sBVy+TugUimHs6wwnepA0E5JYdmMnCw6OOvZDnMziO2SJg5OhXDE\nfFBNp1jA5uYri5J1KkaYnLBns8BGi26GHdVpEa5J+MGb24h04nuU5HfafpMP+Vty\nhXp43tnpsdOf75QNkFg5JR3QNzYsd3J6OkQj6MtLCbiWVUMtcZWYLt/sT9kusAz2\nj96VTrRKZx6p5+JEmPuVqKaWRvTYpN0aJ3YXTilQ+QKBgQDlTjbwDS/obF3VXX4e\nR5fdr1RNQUU0MdPM8tuatwrkKakcgfLZq/9le5f1mwOWKSQokUvZFb0OjAKOqZ43\n5wp1tM/UP6mF59SjJeW1lyf4aSoX4EypVDZassxGw+z+nPryo55s14br0zp+OFkt\n2azC/7S68eEri8tCVscElkcmVQKBgQDfyJP9veT+vf1/HVPlGpdBh8zCkPO3pzWE\nzMNTdIYY0OkOvfTl+4bQ+7DA+T9PN6ubMENVSp0SXcxfrTQBj0ePxapvzGeg7Lwm\ngMkUOTatGjLSzYyGWi5Tc/ynkIvq7PUrXsJ6dqnUiRa15pi9eTeRbfb/PmJps4qw\nFSl3LTYRcwKBgQDMSBbXWtgwVsJtgAIIVb2syDLxNFHDwxQuhLkxpxoz1A2NkRNZ\n6kn4Ddh9/OCAGRzWa0LIHf60g/UPRXrn0JPX8wIn06Nh4PvlcLCpSwVX39CZPsDt\nJVHbEWi9LnTnC9Dg8vUDgCr50s9MfUStfTvU6NsIpM317m9hJU/d/UdblQKBgQCt\n46FDoYRIgn9xf+uJ8iColgqBAbuv8KlAmRAKUFhG/kaq4uZisxFuYLVXLhaIytx5\nCYa4xvLIq8Q8cE0iDSDxUgp/NxkrTeliDd6x7UdUOTBW58wTNg4bYNxppINkuF5G\nJir+mCidcy0HibkUOhX41rcAXYyBUDcrXjZLWgFSiwKBgQCT79Q4GIN0MqJEvQxs\nvUfMF96gsJ9cEGGjROtw/qlpaPJBhpmNSuHc6gKAxLDneIdt5nBxyo+lZOpzMIve\ntwnocYa/D8RCOYBX1S3pecI72ss82N+Ek/o2+PNt3nbZ+wxBhNlOIMiH3LxKOsLM\nuzNty1dOJ97/L0ED5IZ9sESCwg==\n-----END PRIVATE KEY-----\n",
-            "client_email": "temp-606@focused-elysium-340604.iam.gserviceaccount.com",
-            "client_id": "113559229310896807100",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/temp-606%40focused-elysium-340604.iam.gserviceaccount.com"
-        }
 
-        st.session_state.sa = gspread.service_account_from_dict(cred)
 
-        st.session_state.sh = st.session_state.sa.open("KT_DATA_ANKETA")
-        st.session_state.wks = st.session_state.sh.worksheet('result')
-        st.session_state.values_list = list()
+    if st.button(label='отправить') :
+
+        st.session_state.all_input_filled = True
         for i in range(len(q)):
-            key ='a'+str(i)
-            if isinstance(st.session_state[key], datetime.date):
-                st.session_state.values_list.append(st.session_state[key].strftime("%d/%m/%Y"))
-            elif isinstance(st.session_state[key],list):
-                st.session_state.values_list.append(', '.join(map(str, st.session_state[key])))
-            else:
-                st.session_state.values_list.append(st.session_state[key])
+            key = 'a' + str(i)
+            if not isinstance(st.session_state[key],int) and not isinstance(st.session_state[key], datetime.date):
+                if len(st.session_state[key]) == 0:
+                    st.session_state.all_input_filled = False
 
-        #print(values)
-        st.session_state.wks.append_row( st.session_state.values_list)
-        for key in st.session_state.keys():
-            del st.session_state[key]
+        if st.session_state.all_input_filled:
+            st.session_state.wks = st.session_state.sh.worksheet('result')
+            st.session_state.values_list = list()
+            for i in range(len(q)):
+                key ='a'+str(i)
+                if isinstance(st.session_state[key], datetime.date):
+                    st.session_state.values_list.append(st.session_state[key].strftime("%d/%m/%Y"))
+                elif isinstance(st.session_state[key],list):
+                    st.session_state.values_list.append(', '.join(map(str, st.session_state[key])))
+                else:
+                    st.session_state.values_list.append(st.session_state[key])
 
+            st.session_state.wks.append_row( st.session_state.values_list)
+            for key in st.session_state.keys():
+                del st.session_state[key]
 
-        st.write('отправлено')
+            st.success('отправлено')
+            st.experimental_rerun()
+        else:
+
+            st.warning('поле не заполнено')
