2fab5777259151ef5e552c10dc210578477a574e
Aidar <a87077380001@gmail.com>
2022-04-25 01:06:02 +0600

    remove pass page

--- main.py
+++ main.py
@@ -95,35 +95,36 @@ q = {
 
 st.header('Анкета')
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
+# def check_password():
+#     """Returns `True` if the user had the correct password."""
+#
+#     def password_entered():
+#         """Checks whether a password entered by the user is correct."""
+#         if st.session_state["password"] == st.secrets["password"]:
+#             st.session_state["password_correct"] = True
+#             del st.session_state["password"]  # don't store password
+#         else:
+#             st.session_state["password_correct"] = False
+#
+#     if "password_correct" not in st.session_state:
+#         # First run, show input for password.
+#         st.text_input(
+#             "Пароль", type="password", on_change=password_entered, key="password"
+#         )
+#         return False
+#     elif not st.session_state["password_correct"]:
+#         # Password not correct, show input + error.
+#         st.text_input(
+#             "Пароль", type="password", on_change=password_entered, key="password"
+#         )
+#         st.error("😕 пароль не правильный")
+#         return False
+#     else:
+#         # Password correct.
+#         return True
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Пароль", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Пароль", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 пароль не правильный")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if check_password():
+# if check_password():
+if True:
 
     long_running_function()
 
